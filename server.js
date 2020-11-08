e7fd25826b3f1420609f7961a47f0f0e86434c2b
sallytareq <sally.ammous@gmail.com>
2020-11-08 18:29:24 +0200

    age fix

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const DATABASE_URL = process.env.DATABASE_URL;
 const PORT = process.env.PORT || 3000;
 
 //  Dependencies
+
 const methodOverride = require('method-override');
 const superagent = require('superagent');
 const express = require('express');
@@ -38,7 +39,7 @@ client.connect().then(() => {
 app.get('/', homepageFunction);
 app.post('/details', detailsFunction);
 app.post('/save', saveFunction);
-
+app.use('*', errorFunction);
 
 // Handlers
 // home
@@ -48,25 +49,26 @@ function homepageFunction(request, response) {
 
 // details
 function detailsFunction(request, response) {
-  // console.log(request.body);
-  const day = request.body.day.toString();
-  const month = request.body.month.toString();
-  const year = request.body.year.toString();
-  const urlNasa = `https://api.nasa.gov/planetary/apod?date=${year}-${month}-${day}&api_key=${NASA_API_KEY}`;
-  const urlFact = `http://numbersapi.com/${month}/${day}/date?json`;
   let nasaResp;
   let factResp;
+  const day = request.body.day.toString();
+  const month = request.body.month.toString();
+  const yearNasa = yearCheck(request.body.year.toString());
+  const year = request.body.year.toString();
+  const urlNasa = `https://api.nasa.gov/planetary/apod?date=${yearNasa}-${month}-${day}&api_key=${NASA_API_KEY}`;
+  const urlFact = `http://numbersapi.com/${month}/${day}/date?json`;
+
+  let age =  getAge(year+'-'+month+'-'+day);
+  let planet = request.body.planets;
 
   superagent(urlNasa).then((nasaData) => {
     nasaResp = nasaData.body;
-    // console.log(nasaResp);
   }).then(() => {
     superagent(urlFact).then((factData) => {
       factResp = factData.body;
-      // console.log(factResp);
     }).then(() => {
       let birthday = new Birthday(day, month, year, nasaResp, factResp);
-      const responseObject = { birthday: birthday };
+      const responseObject = { birthday: birthday, age:age , planet:planet };
       response.status(200).render('./pages/details.ejs', responseObject);
     });
   }).catch(console.error);
@@ -78,14 +80,9 @@ function saveFunction(request, response) {
   const day = request.body.day.toString();
   const month = request.body.month.toString();
   const year = request.body.year.toString();
-  const nasa = {
-    title: request.body.title,
-    hdurl: request.body.hdurl
-  };
-  const fact = {
-    text: request.body.text,
-    year: request.body.fact_year
-  };
+  const nasa = {title: request.body.title, hdurl: request.body.hdurl};
+  const fact = {text: request.body.text, year: request.body.fact_year};
+
   const search = 'SELECT * FROM birthday WHERE birth_day=$1 AND birth_month=$2 AND birth_year=$3 AND nasa_name=$4 AND nasa_url=$5 AND fact_year=$6 AND fact_text=$7;';
   const insert = 'INSERT INTO birthday (birth_day, birth_month, birth_year, nasa_name, nasa_url, fact_year, fact_text) VALUES($1,$2,$3,$4,$5,$6,$7);';
   let newBirthday = [day, month, year, nasa.title, nasa.hdurl, fact.year, fact.text];
@@ -104,16 +101,6 @@ function saveFunction(request, response) {
   });
 }
 
-app.post('/result', (request, response) => {
-  console.log(request.body);
-  const results = request.body;
-  response.redirect('/details', {
-    age: results.age,
-    planet: results.planets
-  });
-
-});
-app.use('*', errorFunction);
 // *
 function errorFunction(request, response) {
   response.status(404).render('./pages/error.ejs');
@@ -128,4 +115,21 @@ function Birthday(day, month, year, nasaResp, factResp) {
   this.nasa_url = nasaResp.hdurl;
   this.fact_text = factResp.text;
   this.fact_year = factResp.year;
+}
+
+//helper
+function getAge(date){
+  let diff = new Date()-new Date(date);
+  let age = Math.floor(diff/31557600000);
+  return age;
+}
+
+function yearCheck(req){
+  let year;
+  if (Number(req) >= 1995){
+    year = req;
+  } else {
+    year = '1995';
+  }
+  return year;
 }
